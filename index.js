0d523c603e7524940e9c04fb13f9051b227b0cfe
Jeetesh <jeeteshkumar45@gmail.com>
2023-01-05 11:17:49 +0530

    adding staticfiles

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const path=require('path')
 const port=8800;
 const app=express();
 
+
 app.set('view engine','ejs');
  app.set('views',path.join(__dirname,'views'));
 
  app.use(express.urlencoded());
+
+ app.use(express.static('asset'))
 
 
 //  Middleware-1
